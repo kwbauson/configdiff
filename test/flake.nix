6d63475132adbf4ac53729f44de629fe57ac55a8
Keith Bauson <kwbauson@gmail.com>
2026-06-25 12:33:36 -0400

    change test flake inputs to 26.05 releases

--- test/flake.nix
+++ test/flake.nix
@@ -1,9 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    home-manager.url = "github:nix-community/home-manager";
-    nix-darwin.url = "github:nix-darwin/nix-darwin";
-    nixvim.url = "github:nix-community/nixvim";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim/nixos-26.05";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
     { self
